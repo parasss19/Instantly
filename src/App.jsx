6a762c18553ab66064ceca62284e181195ab96ec
Paras <mehtaparas1901@gmail.com>
2025-09-12 16:40:11 +0530

    routing defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import AppLayout from './layout/AppLayout';
+import Home from './pages/Home';
+import Privacy from './pages/Privacy';
+import PageNotFound from './components/PageNotFound';
 
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      element: <AppLayout />,
+
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        }, 
+        {
+          path: '/privacy',
+          element: <Privacy/>
+        },
+
+        //Catch-all route for 404 
+        {
+          path: '*',
+          element: <PageNotFound/>,
+        },  
+      ],
+    },
+  ]);
 
   return (
     <>
-     <h1 className='bg-blue-800'>hello</h1>
+      <RouterProvider router={router}/>
     </>
   )
 }
